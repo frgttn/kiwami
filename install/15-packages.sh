a7aa0660d39365398e237fd3b7c6c931820f79c7
Vladyslav Hryhorenko <hryhorenkovladyslav@outlook.com>
2026-05-04 16:41:02 +0300

    ref!: cleanup

--- install/15-packages.sh
+++ install/15-packages.sh
@@ -3,25 +3,24 @@
 set -euo pipefail
 
 PACKAGES=(
-    alacritty alsa-utils avahi bash-completion bat bluetui brightnessctl btop 
-    btrfs-progs chromium cups cups-browsed cups-filters cups-pdf dkms 
-    docker docker-buildx docker-compose dust egl-wayland exfatprogs eza 
-    fastfetch fcitx5 fcitx5-gtk fcitx5-qt fd ffmpegthumbnailer fontconfig 
+    alacritty alsa-utils bash-completion bat bluetui brightnessctl btop 
+    chromium cups cups-browsed cups-filters cups-pdf dkms 
+    docker docker-buildx docker-compose dust exfatprogs eza 
+    fastfetch fd ffmpegthumbnailer fontconfig 
     fzf git github-cli gnome-calculator gnome-keyring gnome-themes-extra 
     gpu-screen-recorder grim gst-plugin-pipewire gum gvfs-mtp gvfs-nfs 
     gvfs-smb hypridle hyprland hyprland-guiutils hyprlock hyprpicker 
     hyprsunset imagemagick impala imv inetutils inxi iwd jq kernel-modules-hook 
-    kitty kvantum-qt5 lazydocker lazygit less libsecret libyaml libsass 
-    libpulse libqalculate libreoffice-fresh limine llvm mako 
+    kitty kvantum-qt5 lazydocker lazygit less libreoffice-fresh limine llvm ly mako 
     man-db mise mpv nautilus nautilus-python gnome-disk-utility 
     noto-fonts noto-fonts-cjk noto-fonts-emoji nss-mdns neovim pamixer 
-    papers pciutils pipewire pipewire-alsa pipewire-jack pipewire-pulse plasma-login-manager 
-    playerctl plocate plymouth polkit-gnome power-profiles-daemon python-gobject 
+    papers pciutils pipewire pipewire-alsa pipewire-jack pipewire-pulse
+    playerctl plocate plymouth polkit-gnome power-profiles-daemon
     qt5-wayland qt6-wayland ripgrep sassc satty slurp snapper starship 
-    sushi swaybg swayosd system-config-printer tmux ttf-jetbrains-mono-nerd 
+    sushi swaybg swayosd tmux ttf-jetbrains-mono-nerd 
     ufw unzip uwsm waybar wget whois wireless-regdb wireplumber wiremix 
-    wl-clipboard xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde 
-    xdg-desktop-portal-hyprland zed zoxide zram-generator
+    wl-clipboard xdg-desktop-portal xdg-desktop-portal-gtk
+    xdg-desktop-portal-hyprland zed zoxide
 )
 
 sudo pacman -Syu --noconfirm
--- install/50-display-manager.sh
+++ install/50-display-manager.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # Don't use chrootable here as --now will cause issues for manual installs
-sudo systemctl enable plasmalogin
+sudo systemctl enable ly@tty2.service
